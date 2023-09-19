731b6d2c0e5e219f770cced1c35f1121e60d7c5c
Thomas Martinez <tjm7126@rit.edu>
2023-09-19 11:39:57 -0400

    npm initialized and some files created

---
+++ src/cssHandler.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const getStylesheet = (request, response, fileName, fileType) => {
+
+};
+
+// export for server.js
+module.exports.getStylesheet = getStylesheet;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const cssHandler = require('./cssHandler.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/styles.css':
+      cssHandler.getStylesheet(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
